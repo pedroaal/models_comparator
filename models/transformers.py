4da38e37e04bd86e234fa0d4ead223adc8b96f74
Pedro Altamirano <pedroaal04@gmail.com>
2025-05-17 00:37:14 +0000

    modify pipeline

--- models/transformers.py
+++ models/transformers.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -16,28 +15,38 @@ class NegativeValueHandler(BaseEstimator, TransformerMixin):
     return X
 
 
-class WeatherFeatureEngineer(BaseEstimator, TransformerMixin):
+class DatetimeHandler(BaseEstimator, TransformerMixin):
   def fit(self, X, y=None):
     return self
 
   def transform(self, X):
     X = X.copy()
 
-    # Transform datetime column
     X["DATETIME"] = pd.to_datetime(X["DATETIME"], format="%d-%b-%Y %H:%M")
     X = X.assign(Year=X["DATETIME"].dt.year, Month=X["DATETIME"].dt.month, Hour=X["DATETIME"].dt.hour)
     X = X.drop(columns=["DATETIME"])
 
+    return X
+
+
+class RainfallHandler(BaseEstimator, TransformerMixin):
+  def fit(self, X, y=None):
+    return self
+
+  def transform(self, X):
+    X = X.copy()
+
     X["HAS_RAINFALL"] = X["RAINFALL"].apply(lambda x: 1 if x > 0 else 0)
+    X = X.drop(columns=["RAINFALL"])
+
     return X
 
 
 def clean_pipeline():
   return Pipeline(
     [
-      ("negative_fixer", NegativeValueHandler()),
-      ("weather_engineer", WeatherFeatureEngineer()),
-      ("imputer", SimpleImputer(strategy="mean")),
+      ("datetime_handler", DatetimeHandler()),
+      ("rainfall_handler", RainfallHandler()),
       # TODO: implementar un pca
       ("scaler", StandardScaler()),
     ]
